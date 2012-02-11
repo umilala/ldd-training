97e76c1fb85f19bca8bfb9c25ec5e662a6e25b79
umilala <umilala@gmail.com>
2012-02-11 15:43:15 +0800

    add unregister

--- cdata.c
+++ cdata.c
@@ -15,21 +15,51 @@
 #include <asm/io.h>
 #include <asm/uaccess.h>
 
+#define DEV_MAJOR	121
+#define DEV_NAME	"debug"
+
+
+
 static int cdata_open(struct inode *inode, struct file *filp)
 {
 	return 0;
 }
 
+static int cdata_close(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+static int cdata_read(struct file *filp, char *buffer, size_t size, loff_t *offset)
+{
+	return 0;
+}
+
+static int cdata_write(struct file *filp, const char *buffer, size_t size, loff_t *offset)
+{
+	return 0;
+}
+
+
 static struct file_operations cdata_fops = {	
 	open:		cdata_open,
+	release:	cdata_close,
+	write:		cdata_write,
+	read:		cdata_read,
 };
 
 int cdata_init_module(void)
 {
+	if (register_chrdev(DEV_MAJOR, DEV_NAME, &cdata_fops) < 0) {
+		//error
+		return -1;
+	}
+	return 0;
 }
 
 void cdata_cleanup_module(void)
 {
+	unregister_chrdev(DEV_MAJOR, DEV_NAME);
 }
 
 module_init(cdata_init_module);
