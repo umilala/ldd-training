5fa5f0a9334fe1702db732ce81dd4d0d8ba6d342
umilala <umilala@gmail.com>
2012-02-19 12:58:06 +0800

    1. change to jollen version 2. add timer 3. handle schedule

--- cdata.c
+++ cdata.c
@@ -14,195 +14,190 @@
 #include <linux/input.h>
 #include <asm/io.h>
 #include <asm/uaccess.h>
-
 #include "cdata.h"
 
-#define DEV_MAJOR	121
-#define DEV_NAME	"cdata"
-
-#define BUFFER_SIZE	50*240*4
-
-#define MSG(m...) printk(KERN_INFO "CDATA: " m "\n");
+#define BUF_SIZE (12800)
+#define LCD_SIZE (320*240*4)
 
 struct cdata_t {
 	unsigned long *fb;
-	char* pixels;
-	int index;
+	unsigned char *buf;
+	unsigned int index;
+	unsigned int offset;
+	struct timer_list	flush_timer;
+	struct timer_list	sched_timer;
+
+	//DECLARE_WAIT_QUEUE
+	wait_queue_head_t	wq;
 
 };
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
+	int i;
 	int minor;
-	struct cdata_t* cdata;
+	struct cdata_t *cdata;
 
-	printk(KERN_INFO "CDATA: a in open\n");
+	printk(KERN_INFO "CDATA: in open\n");
+
 	minor = MINOR(inode->i_rdev);
 	printk(KERN_INFO "CDATA: minor = %d\n", minor);
 
-	cdata = (struct cdata_t*)kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
-//	 cdata= kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
-
-	cdata->fb= ioremap(0x33f00000, 320*240*4);
-	cdata->pixels = 0;
+	cdata= kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
+	cdata->buf = kmalloc(BUF_SIZE, GFP_KERNEL);
+	cdata->fb = ioremap(0x33f00000, 320*240*4);
 	cdata->index = 0;
+	cdata->offset = 0;
 
-	filp->private_data =  (void*)cdata;
+
+	init_timer(&cdata->flush_timer);
+	init_timer(&cdata->sched_timer);
+
+	init_waitqueue_head(&cdata->wq);
+
+	filp->private_data = (void *)cdata;
+
+	return 0;
+}
+
+static ssize_t cdata_read(struct file *filp, char *buf, size_t size, loff_t *off)
+{
+}
+
+void flush_lcd(unsigned long priv)
+{
+	struct cdata_t *cdata = (struct cdata *)priv;
+	unsigned char *fb;
+	unsigned char *pixel;
+	int index;
+	int offset;
+	int i;
+	int j;
+
+	fb = (unsigned char *)cdata->fb;
+	pixel = cdata->buf;
+	index = cdata->index;
+	offset = cdata->offset;
+
+	for (i = 0; i < index; i++) {
+		writeb(pixel[i], fb+offset);
+		offset++;
+		if (offset >= LCD_SIZE)
+			offset = 0;
+		// Lab
+		for (j = 0; j < 100000; j++);
+	}
+
+	cdata->index = 0;
+	cdata->offset = offset;
+}
+
+void cdata_wake_up(unsigned long priv)
+{
+	struct cdata_t *cdata = (struct cdata *)priv;
+	struct timer_list *sched;
+	wait_queue_head_t *wq;
+printk(KERN_INFO "CDATA: cdata_wake_up\n");
+	sched = &cdata->sched_timer;
+	wq = &cdata->wq;
+
+	wake_up(wq);
+
+	sched->expires = jiffies + 10;
+	add_timer(sched);
+}
+
+static ssize_t cdata_write(struct file *filp, const char *buf, size_t size, loff_t *off)
+{
+	struct cdata_t *cdata = (struct cdata *)filp->private_data;
+	struct timer_list *timer;
+	struct timer_list *sched;
+	unsigned char *pixel;
+	unsigned int index;
+	unsigned int i;
+        wait_queue_head_t *wq;
+	wait_queue_t wait;
+
+
+	pixel = cdata->buf;
+	index = cdata->index;
+	timer = &cdata->flush_timer;
+	sched = &cdata->sched_timer;
+	wq = &cdata->wq;
+
+	for (i = 0; i < size; i++) {
+		if (index >= BUF_SIZE) {
+
+			cdata->index = index;
+			timer->expires = jiffies + 5*HZ;	//5 secs
+			timer->function = flush_lcd;
+			timer->data = (unsigned long)cdata;
+			add_timer(timer);
+
+			sched->expires = jiffies + 10;		//0.1 secs
+			sched->function = cdata_wake_up;
+			sched->data = (unsigned long)cdata;
+			add_timer(sched);
+
+			wait.flags = 0;
+			wait.task = current;
+			add_wait_queue(wq, &wait);
+
+repeat:
+			current->state = TASK_INTERRUPTIBLE;
+			schedule();
+		 
+			index = cdata->index;
+
+			if (index != 0)
+				goto repeat;
+			printk(KERN_INFO "CDATA: exit repeat\n");
+			remove_wait_queue(wq, &wait);
+			del_timer(sched);
+
+		}
+		copy_from_user(&pixel[index], &buf[i], 1);
+		index++;
+	}
+
+	cdata->index = index;
 
 	return 0;
 }
 
 static int cdata_close(struct inode *inode, struct file *filp)
 {
+	struct cdata_t *cdata = (struct cdata *)filp->private_data;
 
-	struct cdata_t* cdata;
-	MSG("cdata_close");
+	flush_lcd((void *)cdata);
+	del_timer(&cdata->flush_timer);
 
-	cdata = (struct cdata_t*)filp->private_data;
-	if (cdata) {
-		flush_pixels(cdata);
-
-		if (cdata->pixels)
-			kfree(cdata->pixels);
-		kfree(cdata);
-	}
-
+	kfree(cdata->buf);
+	kfree(cdata);
 
 	return 0;
 }
 
-static int cdata_read(struct file *filp, char *buffer, size_t size, loff_t *offset)
+static int cdata_ioctl(struct inode *inode, struct file *filp,
+unsigned int cmd, unsigned long arg)
 {
-	MSG("cdata_read");
-	return 0;
-}
-
-static int cdata_ioctl(struct inode* inode, struct file* filp, unsigned int cmd, unsigned long arg) {
+	struct cdata_t *cdata = (struct cdata *)filp->private_data;
+	int n;
 	unsigned long *fb;
 	int i;
-	int n;
 
-	struct cdata_t* cdata = (struct cdata_t*)filp->private_data;
-
-	MSG("cdata_ioctl ...");
-
-	switch(cmd) {
+	switch (cmd) {
 		case CDATA_CLEAR:
+			n = *((int *)arg); // FIXME: dirty
+			printk(KERN_INFO "CDATA_CLEAR: %d pixel\n", n);
 
-			printk(KERN_INFO "CDATA CDATA_CLEAR\n");
-			//Lock
-			fb= cdata->fb;
-			//UnLock
-			for (i=0; i< 320*240; i++) {
-				writel(0x00ffffff, fb++);
-			}	
+			// FIXME: Lock
+			fb = cdata->fb;
+			// FIXME: unlock
+			for (i = 0; i < n; i++)
+				writel(0x00ff00ff, fb++);
 		break;
-		case CDATA_RED:
-			n = *((int*)arg);
-			printk(KERN_INFO "CDATA CDATA_RED: %d\n", n);
-			//Lock
-			fb= cdata->fb;
-			//UnLock
-			for (i=0; i< 320*240; i++) {
-				writel(0x00ff0000, fb++);
-			}	
-		break;
-		case CDATA_GREEN:
-			printk(KERN_INFO "CDATA CDATA_CLEAR\n");
-			//Lock
-			fb= cdata->fb;
-			//UnLock
-			for (i=0; i< 320*240; i++) {
-				writel(0x0000ff00, fb++);
-			}	
-		default:
-			return -1;
 	}
-	return 0;
-}
-
-static void flush_pixels(void* priv) {
-	unsigned char* pixels;
-	unsigned char* fb;
-	int i;
-	struct cdata_t* cdata = (struct cdata_t *)priv;
-
-	MSG("flush_pixels");
-	//Lock
-	fb = (unsigned char*)cdata->fb;
-	//UnLock
-	pixels = cdata->pixels;
-
-	if (pixels==0) {
-		MSG("flush_pixels ERROR, NULL pointer");
-		return;
-	}
-
-	for (i=0; i< /*320*240*/cdata->index; i++) {
-		writeb(pixels[i], fb++);
-	}	
-
-	//kfree(data);
-
-}
-
-static int cdata_write(struct file *filp, const char *buffer, size_t size, loff_t *offset)
-{
-	int i;
-	struct cdata_t* cdata = (struct cdata_t*)filp->private_data;
-	unsigned long* fb;
-	char* pixels = cdata->pixels;
-	int copy_size = size;
-
-	MSG("cdata_write(BEGIN)");
-	if (!pixels) {
-		printk(KERN_INFO "cdata_write, allocate buf\n");
-		pixels= (char*)kmalloc(sizeof(char)*BUFFER_SIZE, GFP_KERNEL);
-		cdata->pixels = pixels;
-		cdata->index = 0;
-	} else {
-		pixels+=cdata->index;
-		if ((cdata->index + size) > BUFFER_SIZE) {
-			printk(KERN_INFO "cdata_write, adjust copy_size: %d\n", copy_size);
-			copy_size = BUFFER_SIZE - cdata->index;
-		}
-	}
-
-	if(!pixels) {
-		printk(KERN_INFO "pixels is NULL\n");
-		return 0;
-	}
-
-	if (copy_size){
-		copy_from_user(pixels, buffer, copy_size);
-		cdata->index += size;
-		printk(KERN_INFO "cdata_write, still waitting buf, index(%d)\n", cdata->index);
-	}
-
-	if (cdata->index >= BUFFER_SIZE) {
-		flush_pixels(cdata);
-		cdata->index = 0;
-	}
-
-
-	/*for (i=0; i<50000000; i++)while(1) {
-		current->state = TASK_INTERRUPTIBLE;
-		//current->state = TASK_UNINTERRUPTIBLE:
-		schedule();
-	}*/
-
-
-
-
-
-
-	MSG("cdata_write(END)");
-	return 0;
-}
-
-static int cdata_flush(struct file * filp) {
-	MSG("cdata_flush");
-	return 0;
 }
 
 static struct file_operations cdata_fops = {
@@ -212,36 +207,37 @@ static struct file_operations cdata_fops = {
 	read: cdata_read,
 	write: cdata_write,
 	ioctl: cdata_ioctl,
-	flush: cdata_flush,
+
 };
 
-static int cdata_init_module(void)
+int cdata_init_module(void)
 {
-
-	unsigned long *fd;
+	unsigned long *fb;
 	int i;
-	/*fd = ioremap(0x33f00000, 320*240*4);
-	for (i=0; i< 320*240; i++) {
-		writel(0x00ff0000, fd++);	//
-	}*/
 
+	fb = ioremap(0x33f00000, 320*240*4);
+	for (i = 0; i < 320*240; i++)
+		writel(0x00ff0000, fb++);
 
 	if (register_chrdev(121, "cdata", &cdata_fops) < 0) {
 		printk(KERN_INFO "CDATA: can't register driver\n");
 		return -1;
 	}
-
-	printk(KERN_INFO "CDATA, cdata_init_module\n");
-
+	printk(KERN_INFO "CDATA: cdata_init_module\n");
 	return 0;
 }
 
-static void cdata_cleanup_module(void)
+void cdata_cleanup_module(void)
 {
-	unregister_chrdev(DEV_MAJOR, DEV_NAME);
+	unregister_chrdev(121, "cdata");
 }
 
 module_init(cdata_init_module);
 module_exit(cdata_cleanup_module);
 
 MODULE_LICENSE("GPL");
+
+
+
+
+
--- test.c
+++ test.c
@@ -1,40 +1,25 @@
 /*
- * Filename: test.c
- */
-
+* Filename: test.c
+*/
 
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/mman.h>
-
 #include "cdata.h"
 
-int main(void) {
-	int fd, fd3;
-	char buffer[256];
-	int num =3;
+int main(void)
+{
+    int fd;
+    int i;
+    char pix[4] = {0x00, 0xff, 0x00, 0xff};
 
-	char pix[4] = {0x00, 0xff, 0x00, 0x00};
-
-
-	fd = open("/dev/cdata", O_RDWR);
-	printf("test: fd %d\n", fd);
-
-	ioctl(fd, CDATA_CLEAR);
-
-
-	for (num=0; num<(50*240); num++) {		/*   */
-		write(fd, &pix, 4);
-	}
-	/*sleep(5);
-
-	ioctl(fd, CDATA_RED, &num);*/
-
-	close(fd);
-/*	fd3 = open("/dev/cdata3", O_RDWR);
-	printf("test: fd3 %d\n", fd3);
-	write(fd3, buffer, 0);
-	close(fd3);*/
+    i = 10000;
+    fd = open("/dev/cdata", O_RDWR);
+    //ioctl(fd, CDATA_CLEAR, &i);
+    while(1) {
+	write(fd, pix, 4);
+    }
+    close(fd);
 }
