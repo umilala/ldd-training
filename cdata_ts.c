5ae997e8b1720cba92125331d74b1f135433398b
umilala <umilala@gmail.com>
2012-03-11 16:45:59 +0800

    1. change data into private data 2. report x,y into bh

--- cdata_ts.c
+++ cdata_ts.c
@@ -19,30 +19,58 @@
 static void cdata_bh(unsigned long);
 DECLARE_TASKLET(my_tasklet, cdata_bh, 0);
 
+
+struct cdata_ts {
+	struct input_dev ts_input;
+	int x;
+	int y;	
+};
+
+
+
+
 static void cdata_ts_handler(int irqflags, void* priv, struct pt_regs* reg) {
+	struct cdata_ts *cdata = (struct cdata_ts *)priv;
+
 	printk(KERN_INFO "cdata_ts_handler\n");
+
+	cdata->x = 100;
+	cdata->y = 100;
+
+	my_tasklet.data = (unsigned long)cdata;
+
 	tasklet_schedule(&my_tasklet);
 	/* FIXME: read (x, y) from ADC */
+	//x = 100;
+	//y = 100;
 }
 
 static void cdata_bh(unsigned long priv) {
+	struct cdata_ts *cdata = (struct cdata_ts *)priv;
+	struct input_dev *dev = &cdata->ts_input;
+
 	printk(KERN_INFO "cdata_ts: down...\n");
 
-
+	input_report_abs(dev, ABS_X, cdata->x);
+	input_report_abs(dev, ABS_Y, cdata->y);
 }
 
-struct input_dev ts_input;
 int ts_input_open(struct input_dev *dev) {
+
+
+
 	printk(KERN_INFO "ts_input_open\n");
 	return 0;
 }
 
 void ts_input_close(struct input_dev* dev) {
-	printk(KERN_INFO, "ts_input_close\n");
+	printk(KERN_INFO "ts_input_close\n");
 }
 
 static int cdata_ts_open(struct inode *inode, struct file *filp)
 {
+	struct cdata_ts *cdata;
+	cdata = kmalloc(sizeof(struct cdata_ts), GFP_KERNEL);
 #if 0	
 	u32 reg;
 	reg = GPGCON;
@@ -62,17 +90,24 @@ static int cdata_ts_open(struct inode *inode, struct file *filp)
 		XP_PST(WAIT_INT_MODE);
 
 	/* Request touch panel IRQ */
-	if (request_irq(IRQ_TC, cdata_ts_handler, 0, "cdata-ts", 0)) {
+	if (request_irq(IRQ_TC, cdata_ts_handler, 0, "cdata-ts", (void *)cdata)) {
 		printk(KERN_ALERT "cdata: request irq failed.\n");
 		return -1;
 	}
 
-
 	 /** handling input device ***/
-	ts_input.open = ts_input_open;
-	ts_input.close = ts_input_close;
+	cdata->ts_input.name = "ts-input";
+	cdata->ts_input.open = ts_input_open;
+	cdata->ts_input.close = ts_input_close;
+	cdata->ts_input.absbit[0] = BIT(ABS_X) | BIT(ABS_Y);
+	cdata->ts_input.evbit[0] = BIT(KEY_ENTER) | BIT(KEY_F5);
 
-	input_register_device(&ts_input);
+	cdata->x =0;
+	cdata->y =0;
+
+	input_register_device(&cdata->ts_input);
+
+	filp->private_data = (void *)cdata;
 
 	printk(KERN_INFO "cdata_ts_open\n");
 	return 0;
@@ -91,6 +126,8 @@ static ssize_t cdata_ts_write(struct file *filp, const char *buf, size_t size, l
 
 static int cdata_ts_close(struct inode *inode, struct file *filp)
 {
+	struct cdata_ts *cdata = (struct cdata_ts *)filp->private_data;
+	kfree(cdata);
 	return 0;
 }
 
